93626ad7ed574ead5028ce7597c5beda8cc7d192
HOTMAN <flintclay22@gmail.com>
2023-10-04 22:27:07 -0400

    w

--- Sistema de nominas/Modelo/Empresa.py
+++ Sistema de nominas/Modelo/Empresa.py
@@ -1,12 +1,16 @@
 from Trabajador import Trabajador
-from  Proyecto import  Proyecto
-from Trabajador_vin import Trabajadorvin
+from Proyecto import Proyecto
+
 
 class Empresa:
     def __init__(self):
-        self.__lista_trabajadores_vin = ()
-        self.__lista_trabajadores_no_vin = ()
+        self.__lista_trabajadores_vin = []
+        self.__lista_trabajadores_no_vin = []
+
     def insertar_trabajador(self, trabajador):
         list_vin = self.__lista_trabajadores_vin
         list_no_vin = self.__lista_trabajadores_no_vin
-        if trabajador.
+        if trabajador.es_vin:
+            list_vin.append(trabajador)
+        else:
+            list_no_vin.append(trabajador)
