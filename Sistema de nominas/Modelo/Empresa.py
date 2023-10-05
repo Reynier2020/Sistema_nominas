bb1b3247ba8a61f49d456df1e6f794b088438d86
HOTMAN <flintclay22@gmail.com>
2023-10-05 09:06:39 -0400

    w

--- Sistema de nominas/Modelo/Empresa.py
+++ Sistema de nominas/Modelo/Empresa.py
@@ -7,6 +7,7 @@ class Empresa:
         self.__lista_trabajadores_vin = []
         self.__lista_trabajadores_no_vin = []
         self.__lista_proyectos = []
+        self.__lita_trabajadores = self.__lista_trabajadores_vin.extend(self.__lista_trabajadores_no_vin)
 
     def insertar_trabajador(self, trabajador):
         list_vin = self.__lista_trabajadores_vin
@@ -15,3 +16,10 @@ class Empresa:
             list_vin.append(trabajador)
         else:
             list_no_vin.append(trabajador)
+
+    def eliminar_trabajador(self,trabajador):
+        lista_trabajadores = list(self.__lita_trabajadores)
+        for e in range(len(lista_trabajadores)):
+            lista_trabajadores.remove(trabajador)
+
+
