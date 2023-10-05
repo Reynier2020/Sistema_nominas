dc829c1cfff7d533753852634f7c696b2e91ff80
HOTMAN <flintclay22@gmail.com>
2023-10-04 21:11:50 -0400

    w

--- Sistema de nominas/Modelo/Trabajador_vin.py
+++ Sistema de nominas/Modelo/Trabajador_vin.py
@@ -4,7 +4,7 @@ from Proyecto import *
 
 class Trabajadorvin(Trabajador):
 
-    def __int__(self, nombre, edad, sexo, fecha_naci, proy, nivel_pro, rol_proy, plan_cump_men, plan_real_cump):
+    def __init__(self, nombre, edad, sexo, fecha_naci, proy, nivel_pro, rol_proy, plan_cump_men, plan_real_cump):
         super().__int__(nombre, edad, sexo, fecha_naci, nivel_pro)
         self.__proy = proy
         self.__rol_proy = rol_proy
