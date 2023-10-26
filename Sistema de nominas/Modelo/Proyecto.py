7e82a0a1b8cb540cdb587bcab52cae2004a302a9
HOTMAN <flintclay22@gmail.com>
2023-10-26 15:23:12 -0400

    w

--- Sistema de nominas/Modelo/Proyecto.py
+++ Sistema de nominas/Modelo/Proyecto.py
@@ -12,8 +12,8 @@ class Proyecto:
         return self.__nom_proy
 
     @nom_proy.setter
-    def set_nom_proy(self, value):
-        return self.__nom_proy == value
+    def nom_proy(self, value):
+        self.__nom_proy = value
 
     @property
     def cliente(self):
@@ -21,36 +21,36 @@ class Proyecto:
 
     @cliente.setter
     def set_cliente(self, value):
-        return self.__cliente == value
+        self.__cliente = value
 
     @property
     def costo(self):
         return self.__costo
 
     @costo.setter
-    def set_costo(self, value):
-        return self.__costo == value
+    def costo(self, value):
+        self.__costo = value
 
     @property
     def fecha_ini(self):
         return self.__fecha_ini
 
     @fecha_ini.setter
-    def set_fecha_ini(self, value):
-        return self.__fecha_ini == value
+    def fecha_ini(self, value):
+        self.__fecha_ini = value
 
     @property
     def fecha_culm(self):
         return self.__fecha_culm
 
     @fecha_culm.setter
-    def set_fecha_culm(self, value):
-        return self.__fecha_culm == value
+    def fecha_culm(self, value):
+        self.__fecha_culm = value
 
     @property
     def pciento_culm(self):
-        return  self.__pciento_culm
+        return self.__pciento_culm
 
     @pciento_culm.setter
-    def set_pciento_culm(self, value):
-        return self.__pciento_culm == value
+    def pciento_culm(self, value):
+        self.__pciento_culm = value
