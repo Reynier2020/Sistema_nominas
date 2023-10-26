011091cf872888e8ba2f5aef92bf18e6111b6e79
HOTMAN <flintclay22@gmail.com>
2023-10-25 21:50:49 -0400

    w

--- Curso Pyqt/tercera app/lanzador.py
+++ Curso Pyqt/tercera app/lanzador.py
@@ -3,7 +3,7 @@ from trabajo_num import *
 
 
 class Lanzador(QtWidgets.QMainWindow):
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
