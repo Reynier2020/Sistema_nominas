d160373ff4f83426433920fbabf9557c67b21847
Reynier <r3yni3r2020@gmail.com>
2023-11-15 21:16:47 -0500

    w

--- Sistema de nominas/Pruebas Unitarias/test.trabajador_vin.py
+++ Sistema de nominas/Pruebas Unitarias/test.trabajador_vin.py
@@ -5,4 +5,5 @@ from Modelo.Trabajador_vin import Trabajadorvin
 class TestUtils(unittest.TestCase):
 
     def setUp(self):
-        self.trab1 = Trabajadorvin("Nico Perez", 25, "mascculino", 7/1/23, "Profesional",  )
+        self.trab1 = Trabajadorvin("Nico Perez", 25, "mascculino", 7/1/2023, "Profesional", "ayudante", 75, 50)
+        self.trab2 = Trabajadorvin("Paco", 42, "masculino", 15/2/3, "obrero calif", "1er ministro", 100, 5)
