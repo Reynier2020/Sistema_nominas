e88f0b0e774015691764b6a4b90c8a259800585d
Reynier <r3yni3r2020@gmail.com>
2023-11-19 13:49:32 -0500

    w

---
+++ Sistema de nominas/BD/insertar_productos.py
@@ -0,0 +1,14 @@
+import MySQLdb
+
+conn = MySQLdb.connect(host="localhost", user="root", password="r3yni3r2020@gmail.com", db="sistema_nominas")
+cursor = conn.cursor()
+
+cursor.execute("""
+insert into productos(id, nombre, cantidad, precio)
+values(101, camara, 100, 15)
+""")
+
+print("una fila insertada en la tabla productos")
+cursor.close()
+conn.commit()
+conn.close()
