d29fd991f6edb147faf5db272e706bbca752b2dd
Reynier <r3yni3r2020@gmail.com>
2023-11-19 14:23:18 -0500

    w

--- Sistema de nominas/BD/insertar_productos.py
+++ Sistema de nominas/BD/insertar_productos.py
@@ -5,7 +5,7 @@ cursor = conn.cursor()
 
 cursor.execute("""
 insert into productos(id, nombre, cantidad, precio)
-values(101, camara, 100, 15)
+values(101, 'camara', 100, 15)
 """)
 
 print("una fila insertada en la tabla productos")
