b28b8a0a8dfe1434216feaa8b631e466c72701a9
Reynier <r3yni3r2020@gmail.com>
2023-11-28 10:36:24 -0500

    w

--- Sistema de nominas/Presentador/lanzador_gestionar.py
+++ Sistema de nominas/Presentador/lanzador_gestionar.py
@@ -1,5 +1,4 @@
 from Vista.UI.gestionar import *
-import sys
 
 
 class LanzadorGest(QtWidgets.QWidget):
@@ -7,11 +6,4 @@ class LanzadorGest(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = Ui_gestionar_ui()
         self.ui.setupUi(self)
-
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    lanzador_gest = LanzadorGest()
-    lanzador_gest.show()
-    sys.exit(app.exec())
 
--- Sistema de nominas/Presentador/lanzador_principal.py
+++ Sistema de nominas/Presentador/lanzador_principal.py
@@ -10,9 +10,15 @@ class Lan:
 
     def iniciar(self):
         appe = QtWidgets.QApplication(sys.argv)
-        self.prin = lanzador_vent_prin.Lanzador(self)
+        self.prin = lanzador_vent_prin.Lanzador()
         self.prin.show()
         appe.exec()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    lanzador = Lan()
+    lanzador.iniciar()
 
 
 
