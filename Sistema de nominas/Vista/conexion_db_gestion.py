a48526eba5c37743ae864a182a8b92a332c0fb8c
Reynier <r3yni3r2020@gmail.com>
2023-11-29 15:40:58 -0500

    w

--- Sistema de nominas/Vista/conexion_db_gestion.py
+++ Sistema de nominas/Vista/conexion_db_gestion.py
@@ -6,6 +6,8 @@ class RegistroDatos:
         self.conn = \
             MySQLdb.connect(host="localhost", user="root", password="r3yni3r2020@gmail.com", db="sistema_nominas")
 
+# VINCULADOS
+
     def insertar_vin(self, nombre, sexo, edad, fech_naci, nivel_pro, proy_vin, rol_en_pro, plan_cump, plan_real):
         cursor = self.conn.cursor()
         sql = """insert into trabajadores_vin(nombre,sexo,edad, fech_naci, nivel_pro, proy_vin, rol_en_pro, plan_cump,
@@ -15,3 +17,20 @@ class RegistroDatos:
         cursor.execute(sql)
         self.conn.commit()
         cursor.close()
+
+    def buscar_trab_vin(self):
+        cur = self.conn.cursor()
+        sql = """select * from trabajadores_vin"""
+        cur.execute(sql)
+        registro = cur.fetchall()
+        return registro
+
+    def eliminar_vin(self, ide):
+        cursor = self.conn.cursor()
+        sql = """delete from trabajadores_vin where id = {}""".format(ide)
+        cursor.execute(sql)
+        a = cursor.rowcount
+        self.conn.commit()
+        cursor.close()
+        return a
+
