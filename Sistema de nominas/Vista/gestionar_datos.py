a5334c48cdd4f640f1a9b1c3ae32c8fc505c2eec
Reynier <r3yni3r2020@gmail.com>
2023-11-29 11:54:31 -0500

    w

--- Sistema de nominas/Vista/gestionar_datos.py
+++ Sistema de nominas/Vista/gestionar_datos.py
@@ -1,5 +1,14 @@
-from Vista.UI import gestionar
+from Vista.UI.gestionar import *
 from PyQt5 import QtWidgets
+import MySQLdb
+
+
 
 class GestionarDatos(QtWidgets.QWidget):
-    def __init__(self):
+    def __init__(self, presentador):
+        self.__presentador = presentador
+        QtWidgets.QWidget.__init__(self)
+        self.ui = Ui_gestionar_ui()
+        self.ui.setupUi(self)
+
+    # VINCULADOS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
