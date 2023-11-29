89c1d53b2b83d02afb7252c398b8dfcf973c350c
Reynier <r3yni3r2020@gmail.com>
2023-11-29 15:05:48 -0500

    w

--- Sistema de nominas/Vista/conexion_db_gestion.py
+++ Sistema de nominas/Vista/conexion_db_gestion.py
@@ -3,7 +3,8 @@ import MySQLdb
 
 class RegistroDatos:
     def __init__(self):
-        self.conn = MySQLdb.connect(host="localhost", user="root", password="r3yni3r2020@gmail.com", db="sistema_nominas")
+        self.conn = \
+            MySQLdb.connect(host="localhost", user="root", password="r3yni3r2020@gmail.com", db="sistema_nominas")
 
     def insertar_vin(self, nombre, sexo, edad, fech_naci, nivel_pro, proy_vin, rol_en_pro, plan_cump, plan_real):
         cursor = self.conn.cursor()
@@ -11,3 +12,6 @@ class RegistroDatos:
          plan_real)
          values('{}','{}','{}','{}','{}','{}','{}','{}','{}')""".format(nombre, sexo, edad, fech_naci, nivel_pro,
                                                                         proy_vin, rol_en_pro, plan_cump, plan_real)
+        cursor.execute(sql)
+        self.conn.commit()
+        cursor.close()
