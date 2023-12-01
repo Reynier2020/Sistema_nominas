87f844fc49989ad477cbf4705be8ab2bf74d79f8
Reynier <r3yni3r2020@gmail.com>
2023-11-30 20:40:06 -0500

    w

--- Sistema de nominas/Vista/conexion_db_gestion.py
+++ Sistema de nominas/Vista/conexion_db_gestion.py
@@ -11,10 +11,10 @@ class RegistroDatos:
     def insertar_vin(self, nombre, sexo, edad, fech_naci, nivel_pro, proy_vin, rol_en_pro, plan_cump, plan_real):
         cursor = self.conn.cursor()
         try:
-            sql = """insert into trabajadores_vin(nombre,sexo,edad, fech_naci, nivel_pro, proy_vin, rol_en_pro, plan_cump,
-            plan_real)
+            sql = """insert into trabajadores_vin(nombre,sexo,edad,
+             fech_naci, nivel_pro, proy_vin, rol_en_pro, plan_cump,plan_real)
             values('{}','{}','{}','{}','{}','{}','{}','{}','{}')""".format(nombre, sexo, edad, fech_naci, nivel_pro,
-                                                                        proy_vin, rol_en_pro, plan_cump, plan_real)
+                                                                           proy_vin, rol_en_pro, plan_cump, plan_real)
             cursor.execute(sql)
         except MySQLdb.Error:
             self.conn.rollback()
@@ -23,17 +23,23 @@ class RegistroDatos:
 
     def buscar_trab_vin(self):
         cur = self.conn.cursor()
-        sql = """select t.id, t.nombre,t.sexo, t.edad, t.fecha_naci,
-                 t.nivel_pro,p.nombre, t.rol_proyecto, t.plan_cump,
-                 t.plan_real from trabajadores_vin t left join proyectos p on p.id = t.proy_vin"""
-        cur.execute(sql)
+        try:
+            sql = """select t.id, t.nombre,t.sexo, t.edad, t.fecha_naci,
+                    t.nivel_pro,p.nombre, t.rol_proyecto, t.plan_cump,
+                    t.plan_real from trabajadores_vin t left join proyectos p on p.id = t.proy_vin"""
+            cur.execute(sql)
+        except MySQLdb.Error:
+            self.conn.rollback()
         registro = cur.fetchall()
         return registro
 
     def eliminar_vin(self, ide):
         cursor = self.conn.cursor()
-        sql = """delete from trabajadores_vin where id = {}""".format(ide)
-        cursor.execute(sql)
+        try:
+            sql = """delete from trabajadores_vin where id = {}""".format(ide)
+            cursor.execute(sql)
+        except MySQLdb.Error:
+            self.conn.rollback()
         a = cursor.rowcount
         self.conn.commit()
         cursor.close()
@@ -41,11 +47,14 @@ class RegistroDatos:
 
     def act_trab_vin(self,  nombre, sexo, edad, fech_naci, nivel_pro, proy_vin, rol_en_pro, plan_cump, plan_real, ide):
         curs = self.conn.cursor()
-        sql = """update trabajadores_vin set nombre = '{}', sexo = '{}', edad = '{}',
-         fech_naci = '{}', nivel_pro = '{}', proy_vin = '{}', rol_en_pro '{}', plan_cump = '{}', plan_real = '{}'
-          where id = '{}' """.format(nombre, sexo, edad, fech_naci, nivel_pro, proy_vin,
-                                     rol_en_pro, plan_cump, plan_real, ide)
-        curs.execute(sql)
+        try:
+            sql = """update trabajadores_vin set nombre = '{}', sexo = '{}', edad = '{}',
+            fech_naci = '{}', nivel_pro = '{}', proy_vin = '{}', rol_en_pro '{}', plan_cump = '{}', plan_real = '{}'
+            where id = '{}' """.format(nombre, sexo, edad, fech_naci, nivel_pro, proy_vin,
+                                       rol_en_pro, plan_cump, plan_real, ide)
+            curs.execute(sql)
+        except MySQLdb.Error:
+            self.conn.rollback()
         a = curs.rowcount
         self.conn.commit()
         curs.close()
@@ -53,8 +62,11 @@ class RegistroDatos:
 
     def buscar_pro(self):
         cursor = self.conn.cursor()
-        sql = """select nombre from proyectos"""
-        cursor.execute(sql)
+        try:
+            sql = """select nombre from proyectos"""
+            cursor.execute(sql)
+        except MySQLdb.Error:
+            self.conn.rollback()
         list_pro = cursor.fetchall()
         cursor.close()
         return list_pro
